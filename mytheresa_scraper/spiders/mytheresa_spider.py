2f566d3e818ff97652d66eabb41814c0b56ab000
Sinan <sinan.ks786@gmail.com>
2024-06-27 16:45:50 +0530

    Extracted product urls from the start url

--- mytheresa_scraper/spiders/mytheresa_spider.py
+++ mytheresa_scraper/spiders/mytheresa_spider.py
@@ -1,9 +1,16 @@
 import scrapy
-import json
 
 
 class MytheresaSpider(scrapy.Spider):
     name = 'mytheresa'
     allowed_domains = ['mytheresa.com']
     start_urls = ['https://www.mytheresa.com/int_en/men/shoes.html']
+
+    def parse(self, response):
+        #Extracting product URLs from the start URL
+        product_urls = response.css('a.item__link::attr(href)').getall()
+        for url in product_urls:
+            # Extracted URLs join with the base URL
+            absolute_url = response.urljoin(url)
+            yield scrapy.Request(url=absolute_url)
 
