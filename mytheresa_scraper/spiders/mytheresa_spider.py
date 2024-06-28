804294a315d9ef2797197be33f94275c4baf7811
Sinan <sinan.ks786@gmail.com>
2024-06-29 00:42:09 +0530

    Implemented Pagination to scrap data from each page and also set download delay

--- mytheresa_scraper/spiders/mytheresa_spider.py
+++ mytheresa_scraper/spiders/mytheresa_spider.py
@@ -2,11 +2,15 @@ import scrapy
 
 class MytheresaSpider(scrapy.Spider):
     name = 'mytheresa'
+    custom_settings = {
+        'ROBOTSTXT_OBEY': False,
+        'DOWNLOAD_DELAY': 1  # Delay between requests in seconds
+    }
     start_urls = ['https://www.mytheresa.com/int_en/men/shoes.html']
 
     def parse(self, response):
         # Extracting product URLs from the start URL
-        product_urls = response.css('div.item.item--sale a::attr(href)').getall()
+        product_urls = response.css('div.item a::attr(href)').getall()
         seen_urls = set()  # To Avoid duplication
 
         for url in product_urls:
@@ -15,6 +19,11 @@ class MytheresaSpider(scrapy.Spider):
                 yield {
                     'product_url': response.urljoin(url)
                 }
+
+        #Handle Pagination
+        next_page = response.css('a.pagination__item.pagination__item__icon.icon__next.pagination__item.pagination__item__icon.icon__next--active::attr(href)').get()
+        if next_page:
+            yield response.follow(next_page, callback=self.parse)
 
         
 
