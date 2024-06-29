218a4d38ce03e7ff4e1e52d6846c74fd05defed9
Sinan <sinan.ks786@gmail.com>
2024-06-29 17:18:29 +0530

    Changed the listing and offer price code and also implemented get price function for error handling

--- mytheresa_scraper/spiders/mytheresa_spider.py
+++ mytheresa_scraper/spiders/mytheresa_spider.py
@@ -6,12 +6,13 @@ class MytheresaSpider(scrapy.Spider):
         'ROBOTSTXT_OBEY': False,
         'DOWNLOAD_DELAY': 1  # Delay between requests in seconds
     }
+    allowed_domains = ["mytheresa.com"]
     start_urls = ['https://www.mytheresa.com/int_en/men/shoes.html']
 
     def parse(self, response):
         # Extracting product URLs from the start URL
         product_urls = response.css('div.item a::attr(href)').getall()
-        seen_urls = set()  # To Avoid duplication
+        seen_urls = set()  # To avoid duplication
 
         for url in product_urls:
             if url not in seen_urls:
@@ -21,8 +22,8 @@ class MytheresaSpider(scrapy.Spider):
                 # } 
                 yield response.follow(url, callback=self.parse_product)
 
-        #Handle Pagination
-        next_page = response.css('a.pagination__item.pagination__item__icon.icon__next.pagination__item.pagination__item__icon.icon__next--active::attr(href)').get()
+        # Handle Pagination
+        next_page = response.css('a.pagination__item.pagination__item__icon.icon__next--active::attr(href)').get()
         if next_page:
             yield response.follow(next_page, callback=self.parse)
 
@@ -32,16 +33,21 @@ class MytheresaSpider(scrapy.Spider):
             'image_url': response.css('div.photocarousel__items img.product__gallery__carousel__image::attr(src)').get(),
             'brand': response.css('div.product__area__branding__designer a::text').get(),
             'product_name': response.css('div.product__area__branding__name::text').get(),
-            'listing_price': response.css('span.pricing__prices__value--original span.pricing__prices__price::text').getall()[1],
-            'offer_price': response.css('span.pricing__prices__value--discount span.pricing__prices__price::text').getall()[1],
+            'listing_price': self.get_price(response, 'span.pricing__prices__value--original span.pricing__prices__price::text'),
+            'offer_price': self.get_price(response, 'span.pricing__prices__value--discount span.pricing__prices__price::text'),
             'discount': response.css('span.pricing__info__percentage::text').get(),
             'product_id': self.extract_item_number(response),
             'sizes': response.css('div.sizeitem span.sizeitem__label::text').getall(),
             'description': self.extract_description(response),
             'other_images': self.extract_other_images(response)
-        }
-       
+        }      
         yield item
+
+    def get_price(self, response, css_selector):
+        prices = response.css(css_selector).getall()
+        if len(prices) > 1:
+            return prices[1]
+        return prices[0] if prices else None
 
     def extract_item_number(self, response):
         # Locate item number 
