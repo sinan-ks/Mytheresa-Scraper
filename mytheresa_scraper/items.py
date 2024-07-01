3b30210625cbfd20a48e01ba0b9b1b97805a36dc
Sinan <sinan.ks786@gmail.com>
2024-07-01 21:09:45 +0530

    Added Database in settings.py file and Enabled pipelines and also implemented items.py file the data field

--- mytheresa_scraper/items.py
+++ mytheresa_scraper/items.py
@@ -7,6 +7,16 @@ import scrapy
 
 
 class MytheresaScraperItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    
+    breadcrumbs = scrapy.Field()
+    image_url = scrapy.Field()
+    brand = scrapy.Field()
+    product_name = scrapy.Field()
+    listing_price = scrapy.Field()
+    offer_price = scrapy.Field()
+    discount = scrapy.Field()
+    product_id = scrapy.Field()
+    sizes = scrapy.Field()
+    description = scrapy.Field()
+    other_images = scrapy.Field()
+    
--- mytheresa_scraper/settings.py
+++ mytheresa_scraper/settings.py
@@ -60,11 +60,17 @@ DEFAULT_REQUEST_HEADERS = {
 #    "scrapy.extensions.telnet.TelnetConsole": None,
 #}
 
+# SQLite database settings
+DATABASE = {
+    'drivername': 'sqlite',
+    'database': 'mytheresa_data.db', 
+}
+
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    "mytheresa_scraper.pipelines.MytheresaScraperPipeline": 300,
-#}
+ITEM_PIPELINES = {
+   "mytheresa_scraper.pipelines.MytheresaScraperPipeline": 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
